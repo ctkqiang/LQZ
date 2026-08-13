da5fe92101634d48c112003930fa2b88b422ecb7
🇨🇳钟智强 『哪吒网络安全』 <johnmelodymel@qq.com>
2026-08-13 22:45:23 +0800

    refactor(main): 移除未使用的GTK相关代码
    - 删除注释掉的gtkmm应用头文件包含
    - 移除注释掉的GUI窗口创建代码
    - 清理关于1.1版本实现功能的注释
    - 保留核心控制器运行逻辑

--- src/main.cc
+++ src/main.cc
@@ -1,18 +1,9 @@
-// #include <gtkmm/application.h>
-
 #include "controller/controller.h"
-// #include "view/gui.h"
 
 int main(int argc, char* argv[]) {
     // 小管家登场~创建控制器并开启整个程序的魔法流程哟
     Controller phoneForensicController;
     phoneForensicController.Run();
-
-    // auto app = Gtk::Application::create("xin.ctkqiang.lqz");
-
-    // return app->make_window_and_run<GUI>(argc, argv);
-
-    // 在1.1版本中实现此功能
 
     return 0x0;
 }
